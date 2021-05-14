cd1f02020eaa2d19e012e846927d2a217bf5d321
匡志斌 <kuangzhibin@live.com>
2021-05-14 13:39:21 +0800

    调整功能

--- script/pushPlus.js
+++ script/pushPlus.js
@@ -1,20 +1,10 @@
+
+/* eslint-disable no-console */
 const axios = require('axios');
 
-module.exports = function pushMsg() {
+module.exports = function pushMsg(title="标题", content="正文内容") {
   const serverUrl = 'http://www.pushplus.plus/send'
   const token = process.env.PUSHPLUS_TOKEN;
-  let title = "掘金创作者中心上线啦！！"
-  let content = `在这里你可以即时看到粉丝的增长；
-
-
-  也可以即时感受到文章阅读量的提升；
-
-  在这里你可以整理自己的文章并创建专栏；
-
-  也可以在第一时间获知官方最新活动！
-
-  你需要的创作可视化数据都在这里，快点击这里（web端）体验吧！`
-
   axios.get(serverUrl, {
     params: {
       token,
@@ -22,10 +12,8 @@ module.exports = function pushMsg() {
       content
     }
   }).then((res) => {
-    // eslint-disable-next-line no-console
     console.log(res.data)
   }).catch(() => {
-    // eslint-disable-next-line no-console
     console.log('发送失败')
   })
 }
---
+++ script/weather.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const axios = require('axios');
+const pushMsg = require('./pushPlus.js');
+
+const url = 'https://devapi.qweather.com/v7/weather/now'
+const location = 101020100 // 101020100 上海
+const key = process.env.HEWEATHER_KEY;
+axios.get(url, {
+  params: {
+    location,
+    key
+  }
+}).then(res => {
+  console.log(res.data)
+  let {fxLink, now} = res.data;
+  let {text, temp, feelsLike, windDir, windScale} = now;
+  // 101020100 上海
+  pushMsg('实时天气-上海',
+  `天气：${text}
+气温：${temp}℃\t\t体感温度：${feelsLike}℃
+风向：${windDir}\t\t风力：${windScale}级
+
+详细信息： <a href="${fxLink}" target="_blank">${fxLink}</a> `);
+}).catch(() => {
+  console.log('天气查询失败')
+})
